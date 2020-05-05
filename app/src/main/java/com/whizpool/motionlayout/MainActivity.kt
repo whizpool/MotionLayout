9c8dfa35cfb90dbe410167c9c3463e651c55f3fc
Asadullah Ilyas <Kokab.waqas@whizpool.com>
2020-05-05 10:08:09 +0500

    Article 1 prepared

--- app/src/main/java/com/whizpool/motionlayout/MainActivity.kt
+++ app/src/main/java/com/whizpool/motionlayout/MainActivity.kt
@@ -6,53 +6,11 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
 {
-    private val totalItems = 2
-    private var currentIndex = 0
-
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        previous.setOnClickListener {
-
-            val nextIndex = currentIndex - 1
-
-            if (nextIndex < 0)
-            {
-                return@setOnClickListener
-            }
-
-            currentIndex = nextIndex
-            changeView()
-        }
-
-        next.setOnClickListener {
-
-            val nextIndex = currentIndex + 1
-
-            if (nextIndex >= totalItems)
-            {
-                return@setOnClickListener
-            }
-
-            currentIndex = nextIndex
-            changeView()
-        }
-
-        changeView()
-    }
-
-    private fun changeView()
-    {
-        frame.removeAllViews()
-
-        val layoutId = when (currentIndex)
-        {
-            0 -> R.layout.layout_1
-            else -> R.layout.layout_2
-        }
-
-        frame.addView(layoutInflater.inflate(layoutId, frame, false))
+        frame.addView(layoutInflater.inflate(R.layout.layout_1, frame, false))
     }
 }
